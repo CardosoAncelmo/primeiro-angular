069c637d4bf45a00a2ecc32dde9da210beb58b79
Ancelmo <aj.cardoso@bol.com.br>
2021-04-17 21:51:29 -0300

    primeiro-angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { TabelaComponent } from './tabela/tabela.component';
+import { GraficoComponent } from './grafico/grafico.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TabelaComponent,
+    GraficoComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule
+      
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/grafico/grafico.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'spa-grafico',
+  templateUrl: './grafico.component.html',
+  styleUrls: ['./grafico.component.css']
+})
+export class GraficoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
